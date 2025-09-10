633e655d569554227df3c67d59f78473ff761bb4
juanpablo26orozco <106635203+juanpablo26orozco@users.noreply.github.com>
2025-09-10 18:06:26 -0500

    user finished 2.0

--- GenomicAppClient/src/main/java/org/UserFile/FastaReader.java
+++ GenomicAppClient/src/main/java/org/UserFile/FastaReader.java
@@ -13,26 +13,36 @@ public class FastaReader {
         return scanner.nextLine().trim();
     }
 
-
     public static String readContent(File fastaFile) throws IOException {
         if (!fastaFile.exists()) {
             throw new IOException("El archivo FASTA no existe: " + fastaFile.getAbsolutePath());
         }
 
-        String content = Files.readString(fastaFile.toPath()).replace("\r", "").trim();
+
+        String content = Files.readString(fastaFile.toPath())
+                .replace("\r", "")
+                .trim();
+
         String[] lines = content.split("\n");
 
         if (lines.length < 2) {
             throw new IOException("Formato FASTA inválido: faltan líneas");
         }
 
-        String header = lines[0].replace(">", "").trim();
-        StringBuilder sequence = new StringBuilder();
 
+        String header = lines[0].trim();
+        if (header.startsWith(">")) {
+            header = header.substring(1).trim();
+        }
+
+
+        StringBuilder sequence = new StringBuilder();
         for (int i = 1; i < lines.length; i++) {
             sequence.append(lines[i].trim());
         }
-
+        System.out.print(header);
+        System.out.print(sequence);
+        // ✅ devolver header y secuencia separados por ";"
         return header + ";" + sequence.toString();
     }
 }
